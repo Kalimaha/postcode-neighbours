e6df1dc9d8c29ba48cd71996e5a70205bb93eda8
Kalimaha <guido.barbaglia@gmail.com>
2018-03-19 21:42:59 +1100

    New layer

--- bin/01_create_db.sh
+++ bin/01_create_db.sh
@@ -16,27 +16,37 @@ echo
 echo
 echo
 
-echo "=================================== 03 - Install PostGIS extension: START"
-docker exec surroundingsuburbs_db_1 psql -c "CREATE EXTENSION postgis;"
-docker exec surroundingsuburbs_db_1 psql -c "CREATE EXTENSION postgis_topology;"
-docker exec surroundingsuburbs_db_1 psql -c "SELECT PostGIS_full_version();"
-echo "==================================== 03 - Install PostGIS extension: DONE"
+echo "======================= 03 - Install PostGIS extension for Test DB: START"
+docker exec surroundingsuburbs_db_1 psql -c "CREATE EXTENSION postgis;" app_test
+docker exec surroundingsuburbs_db_1 psql -c "CREATE EXTENSION postgis_topology;" app_test
+docker exec surroundingsuburbs_db_1 psql -c "SELECT PostGIS_full_version();" app_test
+echo "======================== 03 - Install PostGIS extension for Test DB: DONE"
 
 echo
 echo
 echo
 
-echo "==================================== 04 - Store layer into Test DB: START"
-docker exec surroundingsuburbs_db_1 ogr2ogr -append -f "PostgreSQL" PG:"dbname=app_test" victoria_suburbs.shp -nln victoria_suburbs
-echo "===================================== 04 - Store layer into Test DB: DONE"
+echo "================ 04 - Install PostGIS extension for Development DB: START"
+docker exec surroundingsuburbs_db_1 psql -c "CREATE EXTENSION postgis;" app_development
+docker exec surroundingsuburbs_db_1 psql -c "CREATE EXTENSION postgis_topology;" app_development
+docker exec surroundingsuburbs_db_1 psql -c "SELECT PostGIS_full_version();" app_development
+echo "================= 04 - Install PostGIS extension for Development DB: DONE"
 
 echo
 echo
 echo
 
-echo "============================= 05 - Store layer into Development DB: START"
-docker exec surroundingsuburbs_db_1 ogr2ogr -append -f "PostgreSQL" PG:"dbname=app_development" victoria_suburbs.shp -nln victoria_suburbs
-echo "============================== 05 - Store layer into Development DB: DONE"
+echo "==================================== 05 - Store layer into Test DB: START"
+docker exec surroundingsuburbs_db_1 ogr2ogr -append -f "PostgreSQL" PG:"dbname=app_test" suburbs.shp -nln suburbs
+echo "===================================== 05 - Store layer into Test DB: DONE"
+
+echo
+echo
+echo
+
+echo "============================= 06 - Store layer into Development DB: START"
+docker exec surroundingsuburbs_db_1 ogr2ogr -append -f "PostgreSQL" PG:"dbname=app_development" suburbs.shp -nln suburbs
+echo "============================== 06 - Store layer into Development DB: DONE"
 
 echo
 echo
--- db/schema.rb
+++ db/schema.rb
@@ -14,5 +14,10 @@ ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "postgis"
+  enable_extension "postgis_topology"
+
+# Could not dump table "suburbs" because of following StandardError
+#   Unknown type 'geometry(Polygon,900914)' for column 'wkb_geometry'
 
 end

3ebcffda37ed802c07fbf14960039c6adab9f7e8
Kalimaha <guido.barbaglia@gmail.com>
2018-03-21 09:25:07 +1100

    Store layer in DB through the script

--- bin/create_and_seed_gis_db.sh
+++ bin/create_and_seed_gis_db.sh
@@ -4,17 +4,13 @@ echo "=================================================== 01 - Create DB: START"
 docker-compose run web bundle exec rake db:create
 echo "==================================================== 01 - Create DB: DONE"
 
-echo
-echo
-echo
+yes '' | sed 3q
 
 echo "================================================== 02 - Migrate DB: START"
 docker-compose run web bundle exec rake db:migrate
 echo "=================================================== 02 - Migrate DB: DONE"
 
-echo
-echo
-echo
+yes '' | sed 3q
 
 echo "======================= 03 - Install PostGIS extension for Test DB: START"
 docker exec surroundingsuburbs_db_1 psql -c "CREATE EXTENSION postgis;" app_test
@@ -22,9 +18,7 @@ docker exec surroundingsuburbs_db_1 psql -c "CREATE EXTENSION postgis_topology;"
 docker exec surroundingsuburbs_db_1 psql -c "SELECT PostGIS_full_version();" app_test
 echo "======================== 03 - Install PostGIS extension for Test DB: DONE"
 
-echo
-echo
-echo
+yes '' | sed 3q
 
 echo "================ 04 - Install PostGIS extension for Development DB: START"
 docker exec surroundingsuburbs_db_1 psql -c "CREATE EXTENSION postgis;" app_development
@@ -32,22 +26,18 @@ docker exec surroundingsuburbs_db_1 psql -c "CREATE EXTENSION postgis_topology;"
 docker exec surroundingsuburbs_db_1 psql -c "SELECT PostGIS_full_version();" app_development
 echo "================= 04 - Install PostGIS extension for Development DB: DONE"
 
-echo
-echo
-echo
+yes '' | sed 3q
 
 echo "==================================== 05 - Store layer into Test DB: START"
-docker exec surroundingsuburbs_db_1 ogr2ogr -append -f "PostgreSQL" PG:"dbname=app_test" suburbs.shp -nln suburbs
+docker exec surroundingsuburbs_db_1 ogr2ogr -f "PostgreSQL" PG:"dbname=app_test" suburbs.shp -nln suburbs -nlt PROMOTE_TO_MULTI -overwrite
+docker exec surroundingsuburbs_db_1 psql -c "SELECT COUNT(*) FROM suburbs;" app_test
 echo "===================================== 05 - Store layer into Test DB: DONE"
 
-echo
-echo
-echo
+yes '' | sed 3q
 
 echo "============================= 06 - Store layer into Development DB: START"
-docker exec surroundingsuburbs_db_1 ogr2ogr -append -f "PostgreSQL" PG:"dbname=app_development" suburbs.shp -nln suburbs
+docker exec surroundingsuburbs_db_1 ogr2ogr -f "PostgreSQL" PG:"dbname=app_development" suburbs.shp -nln suburbs -nlt PROMOTE_TO_MULTI -overwrite
+docker exec surroundingsuburbs_db_1 psql -c "SELECT COUNT(*) FROM suburbs;" app_development
 echo "============================== 06 - Store layer into Development DB: DONE"
 
-echo
-echo
-echo
+yes '' | sed 3q
